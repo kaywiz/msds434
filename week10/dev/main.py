2c66f236207df82e3e105a187503eba186c072b3
Kelly Wisneski <k.wisneski@wustl.edu>
2022-11-25 14:41:58 -0600

    Update main.py

--- week10/dev/main.py
+++ week10/dev/main.py
@@ -25,9 +25,10 @@ app = Flask(__name__)
 @app.route('/')
 class MainPage(webapp.RequestHandler):
   def get(self):
-    r = requests.post("https://us-central1-ml.googleapis.com/v1/projects/msds434-final/models/TIP_MODEL/versions/v1:predict?access_token=ya29.a0AeTM1icVPM2w1ra3i4QokHaQPzjEw9jljFZFHOtr-qj4txNtYuM_syjYPKRZq6UVat-_XU_28ibFjBtsUvd_iO2jD5NUL3jcIS078kPby4KDlHp-ahPkB7JcBAQ1Ukc4GaNQq272aRJjmMyjJnFPI0o0tXNcVwaCgYKAaUSARASFQHWtWOmLIe4jHu-Rak4ZmtTMV_9eQ0165", data={"instances": [{"fare": 17.75,"extras": 1.0,"trip_total": 7.45,"payment_type": "Cash"}]})
+    payload = {'key1': 'value1', 'key2': 'value2'}
+    r = requests.post("https://httpbin.org/post", data=payload)
     #self.response.headers['Content-Type'] = 'json'
-    self.response.out.write(r.status_code)
+    self.response.out.write(r.text)
 
 
 if __name__ == '__main__':
