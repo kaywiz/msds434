bf1abc5698a847f07781e671bb44e35ce3413ae2
Kelly Wisneski <k.wisneski@wustl.edu>
2022-11-25 16:12:10 -0600

    Update main.py

--- week10/dev/main.py
+++ week10/dev/main.py
@@ -26,7 +26,7 @@ def hello():
     payload={"instances":[{"fare": 17.75,"extras": 1.0,"trip_total": 7.45,"payment_type": "Cash"}]}
     url='https://us-central1-ml.googleapis.com/v1/projects/msds434-final/models/TIP_MODEL/versions/v1:predict?access_token=ya29.a0AeTM1ieUF8zyGiBDOHZcOsEyuPP1aiwZmGGC_LgGQYdAidGlJBCzyxRi6rHMIxCtbLInTTOnvZ8MySZBzQFQ-umhtgRlGVdFM0YUGQ_RnZDL_dTEpG3qCkgVBYpejAAe3ZpjWrejrMmUPwqBJ3olFpm1iJtd8AaCgYKAWQSARASFQHWtWOmGNnhuzbY5G09ill587oPTA0165'
     r = requests.post(url, json=payload)
-    m = "Payload: "+str(payload)
+    m = "Payload: "+str(payload)+"\n"+str(r.text)
     return m
 
 
