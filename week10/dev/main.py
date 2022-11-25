37f2249c07d71a07834894865f124015c2fa629a
Kelly Wisneski <k.wisneski@wustl.edu>
2022-11-25 14:03:45 -0600

    Update main.py

--- week10/dev/main.py
+++ week10/dev/main.py
@@ -31,10 +31,11 @@ import webapp2
 
 try:
     form_data = urllib.urlencode(UrlPostHandler.form_fields)
+    instances = {"instances": [{"fare": 17.75,"extras": 1.0,"trip_total": 7.45,"payment_type": "Cash"}]}
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
     result = urlfetch.fetch(
-        url='http://localhost:8080/submit_form',
-        payload=form_data,
+        url='https://us-central1-ml.googleapis.com/v1/projects/msds434-final/models/TIP_MODEL/versions/v1:predict?access_token=ya29.a0AeTM1ieYEv4ctIelG7538Q7xcxl4Dmj7JtTF110rB5bzvSud0ojaNPyQybjFBzvt19RR3etbs0fiKt9AgDduhEqf9XcReZlJCQT7LNsgi1rYaNDBIZdtQ_tP_ZjyRRnUVf5LX3RMBhQSk5R9Uds2QICcKJFlaCgYKAWESARASFQHWtWOmEKtKS9KF5-bU7-vRQomksw0163',
+        payload=instances,
         method=urlfetch.POST,
         headers=headers)
     self.response.write(result.content)
