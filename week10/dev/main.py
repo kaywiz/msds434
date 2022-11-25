3799b9a3c570406cb2f6a570d7ebace005376845
Kelly Wisneski <k.wisneski@wustl.edu>
2022-11-25 14:38:18 -0600

    Update main.py

--- week10/dev/main.py
+++ week10/dev/main.py
@@ -26,7 +26,7 @@ app = Flask(__name__)
 class MainPage(webapp.RequestHandler):
   def get(self):
     r = requests.post("https://us-central1-ml.googleapis.com/v1/projects/msds434-final/models/TIP_MODEL/versions/v1:predict?access_token=ya29.a0AeTM1icVPM2w1ra3i4QokHaQPzjEw9jljFZFHOtr-qj4txNtYuM_syjYPKRZq6UVat-_XU_28ibFjBtsUvd_iO2jD5NUL3jcIS078kPby4KDlHp-ahPkB7JcBAQ1Ukc4GaNQq272aRJjmMyjJnFPI0o0tXNcVwaCgYKAaUSARASFQHWtWOmLIe4jHu-Rak4ZmtTMV_9eQ0165", data={"instances": [{"fare": 17.75,"extras": 1.0,"trip_total": 7.45,"payment_type": "Cash"}]})
-    self.response.headers['Content-Type'] = 'json'
+    #self.response.headers['Content-Type'] = 'json'
     self.response.out.write(r.status_code)
 
 
